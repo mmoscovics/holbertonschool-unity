c8f24e7a4b5cd867ac99feab7c2f4074f831790e
Mitchell <mmoscovics@yahoo.com>
2021-01-28 19:14:44 -0500

    [Create] basic character controller

---
+++ 0x05-unity-assets_models_textures/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float move_speed = 10f;
+    public float move_h;
+    public float move_v;
+    public float move_y;
+    public float gravity = 9.8f;
+    public float jump_speed = 3f;
+
+    private CharacterController controller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = gameObject.GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        move_h = Input.GetAxis("Horizontal");
+        move_v = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(move_h, 0, move_v);
+
+
+        if (Input.GetButtonDown("Jump") && controller.isGrounded)
+        {
+            move_y = jump_speed;
+        }
+
+        if (move_y >= -30)
+            move_y -= gravity * Time.deltaTime;
+        direction.y = move_y;
+
+        controller.Move(direction * move_speed * Time.deltaTime);
+    }
+}
